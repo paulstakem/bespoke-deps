72111521669bf5c02d9d8a95c1373a47f27203c6
Paul Stakem <3093926+paulstakem@users.noreply.github.com>
2022-11-01 09:55:11 +0000

    Bump

--- check.sh
+++ check.sh
@@ -5,8 +5,7 @@ VERSION="v2.296.1"
 LATEST=$(curl --silent "https://api.github.com/repos/${REPO}/releases/latest" | jq -r .tag_name)
 if [[ "${LATEST}" > "${VERSION}" ]];
 then
-  echo "Update required...";
-  gh pr create -B main -f -t "Update runner to ${LATEST}!" -b "See github.com/${REPO}/releases/latest"
+  echo "Update available for action/runner ${LATEST}...";
 else
   echo "No update required";
 fi;
