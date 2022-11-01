ba6a614d3a71585a744291ba1ccc42e48a41f5d2
Paul Stakem <3093926+paulstakem@users.noreply.github.com>
2022-11-01 10:05:19 +0000

    Bump

--- get.sh
+++ get.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
+RUNNER_VERSION="v2.297.0"
 
-RUNNER_VERSION=2.297.0
-
-mkdir actions-runner && cd actions-runner
+mkdir actions-runner
+cd actions-runner || return
 curl -O -L https://github.com/actions/runner/releases/download/v${RUNNER_VERSION}/actions-runner-linux-x64-${RUNNER_VERSION}.tar.gz
 tar xzf ./actions-runner-linux-x64-${RUNNER_VERSION}.tar.gz
